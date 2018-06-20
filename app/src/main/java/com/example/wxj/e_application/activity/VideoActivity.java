15d49e436fe5366f0d8d51cda78a1aff5ae11eb9
weixijie <3337183289@qq.com>
2018-06-20 15:58:00 +0800

    001

--- app/src/main/java/com/example/wxj/e_application/activity/VideoActivity.java
+++ app/src/main/java/com/example/wxj/e_application/activity/VideoActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.bumptech.glide.Glide;
 import com.example.wxj.e_application.R;
 
 import cn.jzvd.JZVideoPlayer;
